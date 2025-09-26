f9978c1835c7af2b335efc369905457d28b3217f
xkasprx <daniel@dmskaspr.com>
2025-09-26 11:21:11 -0500

    upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+	const [now, setNow] = useState(new Date());
+
+	useEffect(() => {
+		const timer = setInterval(() => setNow(new Date()), 1000);
+		return () => clearInterval(timer);
+	}, []);
+
+	const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+	const dateString = now.toLocaleDateString([], { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+
+	return (
+		<div className="clock-container">
+			<img className="clock-logo" src="/TC-logo.svg" alt="Taylor County Logo" />
+			<div className="clock-time">{timeString}</div>
+			<div className="clock-date">{dateString}</div>
+		</div>
+	);
+}
+
+export default App;
