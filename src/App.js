322987c1961f71c4946209debf08737c109e5963
xkasprx <daniel@dmskaspr.com>
2025-09-26 13:32:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ function App() {
 	const [now, setNow] = useState(new Date());
 
 	useEffect(() => {
-		const timer = setInterval(() => setNow(new Date()), 1000);
-		return () => clearInterval(timer);
+		const timer = setInterval(() => setNow(new Date()), 1000); // Update every second
+		const timer2 = setInterval(() => setNow(new Date()), 60000); // Force re-render every minute for date update
+		const timer3 = setInterval(() => window.location.reload(), 600000); // Reload every 10 minutes
+		return () => {
+			clearInterval(timer);
+			clearInterval(timer2);
+			clearInterval(timer3);
+		};
 	}, []);
 
 	const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
